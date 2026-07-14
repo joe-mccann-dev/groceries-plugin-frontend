43e2acc8297cbd0c47f66246dc52164e2dad18c5
Joe McCann <joe.mccann@mailbox.org>
2026-07-13 23:19:52 -0400

    Make it possible to add item and item quantity to list.

--- groceries-plugin-frontend/src/components/Main.vue
+++ groceries-plugin-frontend/src/components/Main.vue
@@ -2,7 +2,10 @@
 import { ref, onMounted } from 'vue'
 // const count = ref(0)
 import axios from 'axios'
+
 const items = ref({})
+const groceryToAdd = ref('')
+const groceryQuantity = ref(1)
 const loading = ref(false)
 const error = ref(null)
 
@@ -19,6 +22,16 @@ const fetchItems = async () => {
     }
 }
 
+const addItem = async (groceryToAdd, groceryQuantity) => {
+    try {
+        const response = await axios.post(`http://127.0.0.1:8000/items/${groceryToAdd}/${groceryQuantity}`)
+    } catch(err) {
+        alert(`failed to add item: error ${err.message}`)
+    } finally {
+        fetchItems()
+    }
+}
+
 onMounted(fetchItems)
 </script>
 
@@ -26,17 +39,29 @@ onMounted(fetchItems)
     <section>
         <div>
             <h1>Groceries App</h1>
-            <h2>To Buy</h2>
-
-            <div v-if="loading">loading items . . .</div>
-            <div v-else-if="error">{{ error }}</div>
-            <div v-else>
-                <ul v-for="(list, key, index) in items">
-                    <li v-for="item in list">
-                        <div>{{ item.item_name }} <span>[{{ item.quantity }}]</span></div>
-                    </li>
-                </ul>
-            </div>
+            <section>
+                <h2>To Buy</h2>
+                <div v-if="loading">loading items . . .</div>
+                <div v-else-if="error">{{ error }}</div>
+                <div v-else>
+                    <ul v-for="(list, key, index) in items">
+                        <li v-for="item in list">
+                            <div>{{ item.item_name }} <span>[{{ item.quantity }}]</span></div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
         </div>
+    </section>
+    <section>
+        <h2>Add Item</h2>
+        <div>
+            <label for="groceryToAdd">Item to add to grocery list: {{ groceryToAdd }}</label>
+            <input id="groceryToAdd" v-model="groceryToAdd" placeholder="type item to add here">
+            <label for="groceryQuantity">Quantity (pending): {{ groceryQuantity }}</label>
+            <input id="groceryQuantity" type="number" v-model="groceryQuantity" min="1" max="144">
+            <button @click="addItem(groceryToAdd, groceryQuantity)">Add Item</button>
+        </div>
+
     </section>
 </template>
